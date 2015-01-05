28cfc1b44eebe4121e333540ee938aab04c0ed2e
Matt Memmo <mmemmo@EAMAC3324-10.local>
2015-01-05 14:05:32 -0500

    Initial Commit

---
+++ FlappyBirdEA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlappyBirdEA
+//
+//  Created by Matt Memmo on 1/5/15.
+//  Copyright (c) 2015 MattMemmo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlappyBirdEA/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlappyBirdEA
+//
+//  Created by Matt Memmo on 1/5/15.
+//  Copyright (c) 2015 MattMemmo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
